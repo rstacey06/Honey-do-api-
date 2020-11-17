7eb8fa2307c446fad8103d8b0c146e80870dab3f
github username <github email address>
2020-11-17 07:59:34 -0500

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.delete_all
+
+Todo.create(target:"Remodel Kitchen", plan: "Do whatever my wife wants")
+Todo.create(target:"Grocerys", plan: "Go to the Grocery Store")
+
+Goal.create(description: "Eggs", todo_id: 13, completed: 0)
+Goal.create(description: "Milk", todo_id: 13, completed: 0)
+Goal.create(description: "Bread", todo_id: 13, completed: 0)
+
+Goal.create(description: "Higher a Contractor", todo_id: 12, completed: 0)
+Goal.create
